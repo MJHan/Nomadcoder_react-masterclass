752b57d1d49b132f96d46111a7ede7b5ccddad7c
MJHan <hamatoto@gmail.com>
2023-07-31 01:18:45 +0900

    [Styled-components] animation 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,53 @@
+import styled, { keyframes } from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const rotationAnimation = keyframes`
+  0% {
+    transform:rotate(0deg);
+    border-radius:0px;
+  }
+  50% {
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
+`;
+
+const Emoji = styled.span`
+  font-size: 36px;
+`;
+
+const Box = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 200px;
+  background-color: tomato;
+  animation: ${rotationAnimation} 1s linear infinite;
+  ${Emoji}:hover {
+    font-size: 98px;
+  }
+`;
+
 function App() {
-  return <h1>Hello!</h1>;
+  return (
+    <Wrapper>
+      <Box>
+        <Emoji>🤩</Emoji>
+      </Box>
+      <Emoji>😂</Emoji>
+    </Wrapper>
+  );
 }
 
 export default App;
