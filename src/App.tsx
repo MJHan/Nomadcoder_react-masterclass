d0b9c480fe4a2fd8623c1395f6504f95636c3de8
MJHan <hamatoto@gmail.com>
2023-07-31 04:17:53 +0900

    typescript 적용 소스 정상화

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle borderColor="yellow" bgColor="teal" />
+      <Circle text="im here" bgColor="tomato" />
+    </div>
+  );
+}
+
+export default App;
