e7c35bbf827fbb82ec6fa25d1fba2b844583ef4d
MJHan <hamatoto@gmail.com>
2023-07-31 04:53:45 +0900

    useState & Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import Circle from "./Circle";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Hello", value);
+  };
   return (
     <div>
-      <Circle borderColor="yellow" bgColor="teal" />
-      <Circle text="im here" bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="user name please"
+        ></input>
+        <button>Log In</button>
+      </form>
     </div>
   );
 }
